7739849f3631dbdf60c5d94f07a2394135170b9d
FrancisIris <93911682+FrancisIris@users.noreply.github.com>
2021-11-08 12:38:17 +0000

    My first pieve of C# code

---
+++ Calculator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string n1;
+            string n2;
+            float output=0;
+            string operation;
+
+            Console.WriteLine("Please enter a number");
+            n1 = Console.ReadLine();
+            Console.WriteLine("add, minus, times, divide?");
+            operation =Console.ReadLine();
+            Console.WriteLine("Please enter a number you would like to "+operation+" (by)");
+            n2 = Console.ReadLine();
+            switch (operation)
+            {
+                case "add":
+                    output = Int32.Parse(n1) + Int32.Parse(n2);
+                    break;
+                case "minus":
+                    output = Int32.Parse(n1) - Int32.Parse(n2);
+                    break;
+                case "times":
+                    output = Int32.Parse(n1) * Int32.Parse(n2);
+                    break;
+                case "divide":
+                    output = Int32.Parse(n1) / Int32.Parse(n2);
+                    break;
+            }
+            Console.WriteLine(n1 + " " + operation + " " + n2 + " = "+ string.Format("{0:N2}",output));
+        }
+    }
+}
