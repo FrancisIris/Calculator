da349e1135de79f8bee659a4ad8e991f84010ece
FrancisIris <93911682+FrancisIris@users.noreply.github.com>
2021-11-10 12:47:47 +0000

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -12,55 +12,80 @@ namespace Calculator
             {
                 Console.WriteLine("+, -, * or / ?");
                 operation = Console.ReadLine();
-            } while (operation != "+" && operation != "-" && operation != "*" && operation != "/");
+            } while (!ValidOperatorCheck(operation));
 
-            string numberOfValuesInput;
-            do
-            {
-                Console.WriteLine("How many numbers would you like to {0} together?", operation);
-                numberOfValuesInput = Console.ReadLine();
-            } while (!(int.TryParse(numberOfValuesInput,out int intInput)&& intInput > 1));
+            Console.WriteLine("How many numbers would you like to {0} together?", operation);
+            int numberOfValuesInput = GetUserValue();
 
-            double[] UserValues = new double[int.Parse(numberOfValuesInput)];
-            for (int iterationsComplete = 0; iterationsComplete < int.Parse(numberOfValuesInput); iterationsComplete++)
+            int[] UserValues = new int[numberOfValuesInput];
+            for (int iterationsComplete = 0; iterationsComplete < numberOfValuesInput; iterationsComplete++)
             {
-                string userValue;
-                do
-                {
-                    Console.WriteLine("{0}. Please enter a number: ",iterationsComplete+1);
-                    userValue=Console.ReadLine();
-                } while (!(double.TryParse(userValue, out _)));
-                UserValues[iterationsComplete] = double.Parse(userValue);
+                Console.WriteLine("Value {0}:",iterationsComplete+1);
+                UserValues[iterationsComplete] = GetUserValue();
             }
-            double outputDouble = 0;
+
+            double outputInt = 0;
             switch (operation)
             {
                 case "+":
-                    foreach(double value in UserValues) {
-                        outputDouble += value;
+                    foreach(int value in UserValues) {
+                        outputInt += value;
                     }
                     break;
                 case "-":
-                    foreach (double value in UserValues)
+                    foreach (int value in UserValues)
                     {
-                        outputDouble -= value;
+                        outputInt -= value;
                     }
                     break;
                 case "*":
-                    foreach (double value in UserValues)
+                    foreach (int value in UserValues)
                     {
-                        outputDouble *= value;
+                        outputInt *= value;
                     }
                     break;
                 case "/":
-                    foreach (double value in UserValues)
+                    foreach (int value in UserValues)
                     {
-                        outputDouble /= value;
+                        outputInt /= value;
                     }
                     break;
             }
-            Console.WriteLine("The answer is "+outputDouble);
+            Console.WriteLine("The answer is "+outputInt);
+
+            
 
         }
+        static bool ValidOperatorCheck(string userInput)
+            {
+                switch (userInput)
+                {
+                    case "+":
+                        return true;
+                    case "-":
+                        return true;
+                    case "*":
+                        return true;
+                    case "/":
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+
+            static int GetUserValue()
+            {
+                Console.WriteLine("Enter your value:");
+                string userInput = Console.ReadLine();
+                if (int.TryParse(userInput, out _))
+                {
+                    return int.Parse(userInput);
+                }
+                else
+                {
+                    Console.WriteLine("{0} was not a valid input",userInput);
+                    return GetUserValue();
+                }
+            }
     }
 }
