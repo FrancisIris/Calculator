557dc71383b0ce2b0835747149455e472dd742aa
FrancisIris <93911682+FrancisIris@users.noreply.github.com>
2021-11-10 13:59:38 +0000

    Update Program.cs

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -6,13 +6,23 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
-            string operation;
+            bool runningCheck = true;
+            while (runningCheck)
+            {
+                runningCheck = MainMenu();
+            }
+            return;
+        }
 
+        static void NumberCalculator()
+        {
+            string operation;
+            Console.Clear();
             do
             {
                 Console.WriteLine("+, -, * or / ?");
                 operation = Console.ReadLine();
-            } while (!ValidOperatorCheck(operation));
+            } while (!GetOperator(operation));
 
             Console.WriteLine("How many numbers would you like to {0} together?", operation);
             int numberOfValuesInput = GetUserValue();
@@ -20,7 +30,7 @@ namespace Calculator
             int[] UserValues = new int[numberOfValuesInput];
             for (int iterationsComplete = 0; iterationsComplete < numberOfValuesInput; iterationsComplete++)
             {
-                Console.WriteLine("Value {0}:",iterationsComplete+1);
+                Console.WriteLine("Value {0}:", iterationsComplete + 1);
                 UserValues[iterationsComplete] = GetUserValue();
             }
 
@@ -28,7 +38,8 @@ namespace Calculator
             switch (operation)
             {
                 case "+":
-                    foreach(int value in UserValues) {
+                    foreach (int value in UserValues)
+                    {
                         outputInt += value;
                     }
                     break;
@@ -51,12 +62,23 @@ namespace Calculator
                     }
                     break;
             }
-            Console.WriteLine("The answer is "+outputInt);
-
-            
-
+            Console.WriteLine("The answer is " + outputInt);
+            Console.WriteLine("Enter to continue");
+            Console.ReadLine();
         }
-        static bool ValidOperatorCheck(string userInput)
+
+        static void DateCalculator()
+        {
+            Console.Clear();
+            DateTime userDate = GetDate();
+            Console.WriteLine("How many days would you like to add to this date?:");
+            int daysAdded = GetUserValue();
+            Console.WriteLine("{0} with a {1} day difference ends up as {2}.",userDate.ToShortDateString(), daysAdded, userDate.AddDays(daysAdded).ToShortDateString());
+            Console.WriteLine("Enter to continue");
+            Console.ReadLine();
+        }
+
+        static bool GetOperator(string userInput)
             {
                 switch (userInput)
                 {
@@ -73,7 +95,7 @@ namespace Calculator
                 }
             }
 
-            static int GetUserValue()
+        static int GetUserValue()
             {
                 Console.WriteLine("Enter your value:");
                 string userInput = Console.ReadLine();
@@ -87,5 +109,43 @@ namespace Calculator
                     return GetUserValue();
                 }
             }
+
+        static DateTime GetDate()
+        {
+            Console.WriteLine("Enter the date (dd/mm/yyyy)");
+            string userDateInput = Console.ReadLine();
+            if (DateTime.TryParse(userDateInput,out _))
+            {
+                return DateTime.Parse(userDateInput);
+            }
+            else
+            {
+                Console.WriteLine("{0} was not a valid date format", userDateInput);
+                return GetDate();
+            }
+        }
+
+        
+        static bool MainMenu()
+        {
+            Console.Clear();
+            Console.WriteLine("Mode options:");
+            Console.WriteLine("1) Numbers");
+            Console.WriteLine("2) Dates");
+            Console.WriteLine("3) Exit");
+            string result = Console.ReadLine();
+            switch (result)
+            {
+                case "1":
+                    NumberCalculator();
+                    break;
+                case "2":
+                    DateCalculator();
+                    break;
+                case "3":
+                    return false;
+            }
+            return true;
+        }
     }
 }
